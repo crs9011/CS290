94734e3d9ee195fd84b260d0d64aafaba53eeaea
crs9011 <denninch@onid.orst.edu>
2016-05-30 13:38:30 -0700

    Workout1

--- Workout/script.js
+++ Workout/script.js
@@ -112,5 +112,5 @@ app.use(function(err, req, res, next){
 });
 
 app.listen(app.get('port'), function(){
-	console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+	console.log('Express started on 52.37.44.63:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
